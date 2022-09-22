70c2661f3cc4fb7778d6a1799af09a4dc8bbb0f6
Josue Rodriguez <josue.rodriguez594@gmail.com>
2022-09-22 09:36:17 -0700

    switch to cmdstanr

--- models/hierarchical_multinormal_lapsing_psychometric/stan_files/test-mlm.R
+++ models/hierarchical_multinormal_lapsing_psychometric/stan_files/test-mlm.R
@@ -2,6 +2,15 @@ library(lme4)
 library(brms)
 library(rstan)
 library(dplyr)
+library(cmdstanr)
+
+# for restarting sessions within vscodeß
+radian_restart <- function(args = NULL) {
+  getOption("rchitect.py_tools")$attach()
+  os <- import("os")
+  sys <- import("sys")
+  os$execv(sys$executable, c("sys$executable", "-m", "radian", args))
+}
 
 dat_raw <- read.csv("ace_thresholds_data.csv")
 
@@ -36,8 +45,6 @@ X <- model.matrix(~ avg_rw + pe + consistency + thresh, data = dat)
 
 p <- ncol(X)
 
-
-
 # bprior <- c(prior(normal(1,2), class = b, coef = pe))
 stancode <- make_stancode(
     rt ~ avg_rw + pe + consistency + thresh + timepoint + (timepoint|pid),
@@ -46,12 +53,27 @@ stancode <- make_stancode(
 stancode
 
 standata <- make_standata(
-    rt ~ avg_rw + pe + consistency + thresh + timepoint + (timepoint|pid),
+    rt ~ avg_rw + pe + consistency + thresh + timepoint + (pe|pid),
     data = dat
 )
 str(standata)
 
 
 path <- 
-    "/Users/josue/GitHub/benchmark_stan_models/models/hierarchical_multinormal_lapsing_psychometric/stan_files"
-mod <- stan("stan_files/brms.stan", data = standata, chains = 4, iter = 5000)
+    c("~/GitHub/benchmark_stan_models/models/hierarchical_multinormal_lapsing_psychometric/stan_files/")
+# mod <- stan("brms.stan", data = standata, chains = 4, iter = 5000)
+ 
+
+# ~/.cmdstan/cmdstan-2.30.1/bin/stanc stan_files/brms.stan --include-paths ./
+cmd_mod <- cmdstan_model("stan_files/brms.stan", force_recompile = TRUE, include_paths = "./stan_files")
+
+cmd_fit <- fit <- cmd_mod$sample(
+  data = standata, 
+  seed = 123, 
+  chains = 4, 
+  parallel_chains = 4,
+  refresh = 500 # print update every 500 iters
+)
+
+# mod2 <- brm(rt ~ avg_rw + pe + consistency + thresh + timepoint + (pe|pid), data = dat, chains = 4, iter = 5000)
+# cmd_mod$include_paths()
