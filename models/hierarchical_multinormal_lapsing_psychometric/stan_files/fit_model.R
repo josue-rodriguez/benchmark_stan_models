ab556c09c4690a0a0bd1d20489d3680d96970593
Josue Rodriguez <josue.rodriguez594@gmail.com>
2023-01-07 10:05:35 -0800

    clean up stan_files

--- models/hierarchical_multinormal_lapsing_psychometric/stan_files/fit_model.R
+++ models/hierarchical_multinormal_lapsing_psychometric/stan_files/fit_model.R
@@ -26,7 +26,7 @@ dat <-
         consistency = 1 - cv,
         avg_rw = mean(rw),
         thresh = mean(norm)) |>
-        # slice_sample(prop = 0.10) |>
+        slice_sample(prop = 0.10) |>
     ungroup() |>
     select(pid, rt, timepoint, grade, acc:thresh)
 
@@ -38,19 +38,6 @@ y <- dat$rt
 X <- model.matrix(~ avg_rw + pe + consistency + thresh, data = dat)
 
 p <- ncol(X)
-
-# bprior <- c(prior(normal(1,2), class = b, coef = pe))
-stancode <- make_stancode(
-    rt ~ avg_rw + pe + consistency + thresh + timepoint + (timepoint|pid),
-    data = dat
-)
-stancode
-
-standata <- make_standata(
-    rt ~ avg_rw + pe + consistency + thresh + timepoint + (pe|pid),
-    data = dat
-)
-str(standata)
 
 
 ## --- Psychometric DATA ----
@@ -84,12 +71,7 @@ stanData <- list(
 stan_data <- c(standata, stanData)
 
 
- 
-# system("~/.cmdstan/cmdstan-2.30.1/bin/stanc stan_files/brms.stan --include-paths ./stan_files")
-
-
-# cmd_mod <- cmdstan_model(exe_file = "stan_files/brms", force_recompile = TRUE, include_paths = "./stan_files")
-cmd_mod <- cmdstan_model("stan_files/brms.stan", force_recompile = TRUE, include_paths = "./stan_files")
+cmd_mod <- cmdstan_model("brms.stan", force_recompile = FALSE, include_paths = "./")
 
 cmd_fit <- cmd_mod$sample(
   data = stan_data, 
@@ -99,8 +81,6 @@ cmd_fit <- cmd_mod$sample(
   iter_sampling = 100,
   refresh = 100 # print update every 500 iters
 )
-
+    
 summ <- cmd_fit$summary()
 summ
-# mod2 <- brm(rt ~ avg_rw + pe + consistency + thresh + timepoint + (pe|pid), data = dat, chains = 4, iter = 5000)
-# cmd_mod$include_paths()
